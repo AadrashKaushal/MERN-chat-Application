35927f806652041c0fa15b531eb350e21b12f440
AadrashKaushal <17032000532@gndu.ac.in>
2024-06-17 15:08:28 +0530

    Implementing signup page

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,32 +1,117 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
+    let [switcher,setSwitcher] = useState(true);
+
+    let loginRef = useRef();
+    let signupRef = useRef();
+
+    let loginPasswordRef = useRef();
+    let loginPasswordBtnRef = useRef();
+
+    let showPasswordLoginRef = useRef();
+    let showPasswordBtnRef = useRef();
+
+    let showConfirmPasswordLoginRef = useRef();
+    let showConfirmPasswordBtnRef = useRef();
+
+    const handleSwitching = (e) => {
+        console.log(e.target.id)
+        if(e.target.id === "login") {
+            setSwitcher(true);
+            signupRef.current.style.backgroundColor = "white"
+            loginRef.current.style.backgroundColor = "#bfdbfe"
+
+        } else {
+            setSwitcher(false);
+            signupRef.current.style.backgroundColor = "#bfdbfe"
+            loginRef.current.style.backgroundColor = "white"
+        }
+    }
+
+    const showPassword = (e) => {
+        if(e.target.innerText === "Show") {
+            showPasswordLoginRef.current.type = "text"
+            showPasswordBtnRef.current.innerText = "Hide"
+        } else {
+            showPasswordLoginRef.current.type = "password"
+            showPasswordBtnRef.current.innerText = "Show"
+        }
+    }
+
+    const showConfirmPassword = (e) => {
+        if(e.target.innerText === "Show") {
+            showConfirmPasswordLoginRef.current.type = "text"
+            showConfirmPasswordBtnRef.current.innerText = "Hide"
+        } else {
+            showConfirmPasswordLoginRef.current.type = "password"
+            showConfirmPasswordBtnRef.current.innerText = "Show"
+        }
+    }
+
+    const displayPassword = (e) => {
+        if(e.target.innerText === "Show") {
+            loginPasswordRef.current.type = "text"
+            loginPasswordBtnRef.current.innerText = "Hide"
+        } else {
+            loginPasswordRef.current.type = "password"
+            loginPasswordBtnRef.current.innerText = "Show"
+        }
+    }
+
     return (
         <>
             <center>
-                <div className=" space-y-6 mt-20">
+                <div className=" space-y-6 mt-8">
                     <div className=" w-[30rem] h-20 bg-white rounded-lg p-4">
                         <h1 className="text-3xl font-normal text-gray-600">Talk-A-Tive</h1>
                     </div>
-                    <div className="w-[30rem] h-96 bg-white rounded-lg pt-8">
+                    <div className="w-[30rem] bg-white rounded-lg pt-8">
                         <div>
                             <div className=" flex space-x-12 ml-6 mr-6">
-                                <h1 className="text-base w-48 p-1 rounded-2xl bg-blue-200 cursor-pointer">Login</h1>
-                                <h1 className="text-base w-48 p-1 rounded-2xl cursor-pointer">Sign Up</h1>
+                                <h1 className="text-base w-48 p-1 rounded-2xl bg-blue-200 cursor-pointer" id="login" onClick={handleSwitching} ref={loginRef}>Login</h1>
+                                <h1 className="text-base w-48 p-1 rounded-2xl cursor-pointer" id="signup" onClick={handleSwitching} ref={signupRef}>Sign Up</h1>
                             </div>
                             <div className="flex">
-                                <form className="ml-6 mr-6 mt-8 space-y-3">
-                                    <h1 className="text-sm font-semibold flex">Email Address <span className="text-red-600">*</span></h1>
-                                    <input type="email" className="w-[26rem]  text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="name@example.com" />
-                                    <h1 className="text-sm font-semibold flex">Password <span className="text-red-600">*</span></h1>
-                                    <input type="password" className="w-[26rem] text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="Enter Password" />
-                                    <div>
-                                        <button type="button" className="text-center absolute text-xs w-12 h-6 font-semibold mt-[-2.8rem] ml-[9.5rem] p-1 rounded-md bg-gray-100">Show</button>
-                                    </div>
-                                    <div>
-                                        <button type="button" className="w-[26rem] text-center mt-6 text-white h-10 rounded-md hover:bg-blue-500 bg-blue-400">Login</button>
-                                    </div>
-                                </form>
+
+                                {
+                                    switcher ?
+                                        <form className="ml-6 mr-6 mt-8 space-y-3">
+                                            <h1 className="text-sm font-semibold flex">Email Address <span className="text-red-600">*</span></h1>
+                                            <input type="email" className="w-[26rem]  text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="name@example.com" />
+                                            <h1 className="text-sm font-semibold flex">Password <span className="text-red-600">*</span></h1>
+                                            <input type="password" ref={loginPasswordRef} className="w-[26rem] text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="Enter Password" />
+                                            <div>
+                                                <button type="button" ref={loginPasswordBtnRef} onClick={displayPassword} className="text-center absolute text-xs w-12 h-6 font-semibold mt-[-2.8rem] ml-[9.5rem] p-1 rounded-md bg-gray-100">Show</button>
+                                            </div>
+                                            <div>
+                                                <button type="button" className="w-[26rem] text-center mt-6 mb-4 text-white h-10 rounded-md hover:bg-blue-500 bg-blue-400">Login</button>
+                                            </div>
+                                        </form> 
+                                        :
+                                        <form className="ml-6 mr-6 mt-8 space-y-3">
+                                            <h1 className="text-sm font-semibold flex">Name <span className="text-red-600">*</span></h1>
+                                            <input type="text" className="w-[26rem]  text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="Enter Your Name" />
+                                            <h1 className="text-sm font-semibold flex">Email Address <span className="text-red-600">*</span></h1>
+                                            <input type="email" className="w-[26rem]  text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="name@example.com" />
+                                            <h1 className="text-sm font-semibold flex">Password <span className="text-red-600">*</span></h1>
+                                            <input type="password" ref={showPasswordLoginRef} className="w-[26rem] text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="Enter Password" />
+                                            <div>
+                                                <button type="button" className="text-center absolute text-xs w-12 h-6 font-semibold mt-[-2.8rem] ml-[9.5rem] p-1 rounded-md bg-gray-100" onClick={showPassword} ref={showPasswordBtnRef}>Show</button>
+                                            </div>
+                                            <h1 className="text-sm font-semibold flex">Confirm Password <span className="text-red-600">*</span></h1>
+                                            <input type="password" ref={showConfirmPasswordLoginRef} className="w-[26rem] text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" placeholder="Confirm Password" />
+                                            <div>
+                                                <button type="button" className="text-center absolute text-xs w-12 h-6 font-semibold mt-[-2.8rem] ml-[9.5rem] p-1 rounded-md bg-gray-100" onClick={showConfirmPassword} ref={showConfirmPasswordBtnRef}>Show</button>
+                                            </div>
+                                            <h1 className="text-sm font-semibold flex">Upload Your Picture <span className="text-red-600">*</span></h1>
+                                            <input type="File" className="w-[26rem] text-sm h-10 pb-1 rounded-md pl-3 border focus:outline-none" />
+                                            <div>
+                                                <button type="button" className="w-[26rem] text-center mt-3 mb-3 text-white h-10 rounded-md hover:bg-blue-500 bg-blue-400">Sign Up</button>
+                                            </div>
+                                        </form>
+                                }
                             </div>
                         </div>
                     </div>
